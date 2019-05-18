ddc2f81f5029b5d6539ace665e5b046f47646a9d
olehmv <oleg19850405@hotmail.com>
2019-05-18 17:34:56 +0300

    retry update

--- src/main/scala/scala.camp/utils/Retry.scala
+++ src/main/scala/scala.camp/utils/Retry.scala
@@ -1,6 +1,7 @@
 package scala.camp.utils
 
 import scala.annotation.tailrec
+import scala.concurrent
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
@@ -8,7 +9,9 @@ import scala.concurrent.duration.FiniteDuration
 trait Retry {
 
   @tailrec
-  final def  retry[A](block: () => A, acceptResult: A => Boolean, retries: List[FiniteDuration]): A = {
+  final def retry[A](block: () => A,
+                     acceptResult: A => Boolean,
+                     retries: List[FiniteDuration]): A = {
 
     val a = block()
     if (acceptResult(a)) {
@@ -23,17 +26,19 @@ trait Retry {
       }
     }
   }
-
-  def retryf[A](block: () => Future[A], acceptResult:Future[A] => Boolean, retries: List[FiniteDuration]): Future[A] = {
-    val a = block()
-    if (acceptResult(a)) {
-      a
-    } else {
-      retries match {
-        case Nil => a
-        case x :: xs => {
-          Thread.sleep(x.toMillis)
-          retryf(block, acceptResult, xs)
+  def retryf[A](block: () => Future[A],
+                acceptResult: A => Boolean,
+                retries: List[FiniteDuration]): Future[A] = {
+    block().flatMap { a =>
+      if (acceptResult(a)) {
+        Future.successful(a)
+      } else {
+        retries match {
+          case Nil => Future.successful(a)
+          case x :: xs => {
+            Thread.sleep(x.toMillis)
+            retryf(block, acceptResult, xs)
+          }
         }
       }
     }
