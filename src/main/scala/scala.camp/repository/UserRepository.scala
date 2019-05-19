f03a7ae8c99ee1a282dd5ff5c799e1db2dae24b7
olehmv <oleg19850405@hotmail.com>
2019-05-19 15:43:52 +0300

    added validation and retry

--- src/main/scala/scala.camp/repository/UserRepository.scala
+++ src/main/scala/scala.camp/repository/UserRepository.scala
@@ -1,17 +1,17 @@
 package scala.camp.repository
 
-import slick.lifted.Tag
 import slick.jdbc.H2Profile.api._
+import slick.lifted.Tag
 
 import scala.camp.model.User
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-trait UserRepository{
+trait UserRepository {
 
   lazy val db = Database.forConfig("database")
   lazy val userTable = TableQuery[UserTable]
-  Await.result(db.run(userTable.schema.create),2.seconds)
+  Await.result(db.run(userTable.schema.create), 2.seconds)
 
   class UserTable(tag: Tag) extends Table[User](tag, "users") {
     val id = column[Int]("id", O.PrimaryKey, O.AutoInc)
@@ -22,11 +22,11 @@ trait UserRepository{
     def * = (id, userName, address, email).mapTo[User]
   }
 
-  def getById(id:Int):Future[Option[User]] = {
+  def getById(id: Int): Future[Option[User]] = {
     db.run(userTable.filter(e => e.id === id).result.headOption)
   }
 
-  def registerUser(user:User):Future[Int] = {
+  def registerUser(user: User): Future[Int] = {
     db.run(userTable returning userTable.map(_.id) += user)
   }
 
--- src/main/scala/scala.camp/routes/UserRoutes.scala
+++ src/main/scala/scala.camp/routes/UserRoutes.scala
@@ -1,35 +1,71 @@
 package scala.camp.routes
 
 import akka.http.scaladsl.model.StatusCodes.NotFound
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 
 import scala.camp.model.{JsonSupport, User}
 import scala.camp.repository.UserRepository
-import akka.http.scaladsl.model.StatusCodes
+import scala.camp.utils.{Retry, Validator}
+import scala.concurrent.Future
+import scala.concurrent.duration._
 
+trait UserRoutes extends UserRepository with JsonSupport with Retry {
 
-trait UserRoutes
-  extends UserRepository with JsonSupport {
+  private val emailRegex =
+    """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
+
+  def checkEmail(e: String): Boolean = e match {
+    case null                                          => false
+    case e if e.trim.isEmpty                           => false
+    case e if emailRegex.findFirstMatchIn(e).isDefined => true
+    case _                                             => false
+  }
+
+  def emailValidator = new Validator[String] {
+    override def validate(value: String): Either[String, String] =
+      if (checkEmail(value)) Right(value) else Left(s"Email '$value' is Not valid!")
+  }
+
+  def retryUserPost(user: User): Future[Int] =
+    retryf[Int](() => registerUser(user), id => id != 0, List(1.seconds, 2.seconds, 3.seconds))
+
+  def retryUserGet(id: Int): Future[Option[User]] =
+    retryf[Option[User]](() => getById(id),
+                         user => user.isDefined,
+                         List(1.seconds, 2.seconds, 3.seconds))
+
+  implicit def myExceptionHandler = ExceptionHandler {
+    case e: Throwable => {
+      println(e.getMessage)
+      println(e.getStackTrace)
+      complete(HttpResponse(StatusCodes.BadRequest, entity = e.getMessage))
+    }
+  }
 
   val routes: Route = pathPrefix("user") {
     post {
       entity(as[User]) { user =>
-        onSuccess(registerUser(user)) { i =>
-          complete(StatusCodes.Created,user.copy(id = i))
+        emailValidator.validate(user.email) match {
+          case Right(v) =>
+            onSuccess(retryUserPost(user)) { i =>
+              complete(StatusCodes.Created, user.copy(id = i))
+            }
+          case Left(v) => complete(StatusCodes.BadRequest, v)
         }
       }
-    }~
-    get {
-      parameters('id) {id =>
-        onSuccess(getById(id.toInt)) { user =>
-          user match {
-            case Some(x) => complete(x)
-            case None => complete(NotFound)
+    } ~
+      get {
+        parameters('id) { id =>
+          onSuccess(retryUserGet(id.toInt)) { user =>
+            user match {
+              case Some(x) => complete(x)
+              case None    => complete(NotFound)
+            }
           }
         }
       }
-    }
   }
 
 }
